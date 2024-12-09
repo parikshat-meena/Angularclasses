443fd075ddb95a34e9e1bdd3a0ecb29ca970d494
parikshat meena <parikshat.meena@cordiso.com>
2024-12-09 21:00:56 +0530

    reactive template forms

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  MinValidator,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
-  styleUrl: './reactive-form.component.css'
+  styleUrl: './reactive-form.component.css',
 })
 export class ReactiveFormComponent {
+  myForm = new FormGroup({
+    fname: new FormControl('vaishnavi', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+  });
 
+  OnSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value, 'value of reactive form ');
+    } else {
+      console.log(this.myForm, 'error from forms ');
+    }
+  }
 }
